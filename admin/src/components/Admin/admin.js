6c45b5f73f6ea42aa6cd4bfdd682dd2b734ac096
bhavyaSirikonda <152253309+bhavyaSirikonda@users.noreply.github.com>
2025-01-27 18:02:28 -0500

    admin.js

---
+++ admin/src/components/Admin/admin.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import bg from '../../resources/bg.png'
+
+const AdminHome = () => {
+
+    const backgroundStyle = {
+
+        minHeight: '93.5vh', // Make the background cover the entire viewport height
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+      };
+
+    const titleStyle = {
+        fontSize: '72px', // Change the font size as desired
+        textShadow: '2px 2px 4px rgba(0, 0, 0, 0.8)'
+      };
+
+    return (
+        <div className="home-page" style={backgroundStyle}>
+            <h1 style={titleStyle}>Welcome Admin</h1>
+        </div>
+    );
+};
+
+export default AdminHome;
